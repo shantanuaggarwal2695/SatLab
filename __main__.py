51eaef243b73d5a5f78a622c9cf66cd40e6b9ab2
shantanuaggarwal <shantanuaggarwal29@gmail.com>
2021-10-30 17:48:01 -0400

    egg file change

--- __main__.py
+++ __main__.py
@@ -1,5 +1,31 @@
+from pyspark.sql import SparkSession
+from sedona.utils import SedonaKryoRegistrator, KryoSerializer
+from sedona.register import SedonaRegistrator
+
+def initiate_session():
+    spark = SparkSession. \
+        builder. \
+        master("spark://EN4119507L.cidse.dhcp.asu.edu:7077"). \
+        appName("geo_labeling_functions"). \
+        config("spark.serializer", KryoSerializer.getName). \
+        config("spark.kryo.registrator", SedonaKryoRegistrator.getName). \
+        config("spark.driver.memory", "10g"). \
+        config("spark.executor.memory", "15g"). \
+        config("spark.driver.maxResultSize", "5g"). \
+        config("spark.network.timeout", "1000s"). \
+        config("spark.kryoserializer.buffer.max", "1024"). \
+        config("spark.sql.broadcastTimeout", "36000"). \
+        config("spark.sql.crossJoin.enabled", "true"). \
+        getOrCreate()
+    SedonaRegistrator.registerAll(spark)
+    sc = spark.sparkContext
+    sc.addPyFile("/hdd2/shantanuCodeData/SatLab/dist/experiment-0.1.0-py3.7.egg")
+
+    return spark
+
 
 if __name__ == '__main__':
-    import spark_job.start as start
-    spark = start.initiate_session()
+
+    spark = initiate_session()
+    from spark_job import start
     start.run_experiment(spark)
