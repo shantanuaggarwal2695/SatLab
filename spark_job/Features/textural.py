dde707ab6af76493c7a76de4dde1ac9d3b48f78f
Shantanu Aggarwal (Student) <saggar24@en4102946l.fulton.ad.asu.edu>
2021-11-22 16:44:15 -0700

    textural_functions

--- spark_job/Features/textural.py
+++ spark_job/Features/textural.py
@@ -1,9 +1,13 @@
+from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, IntegerType, DoubleType
 import numpy as np
 from skimage.feature import *
 from skimage.feature import greycoprops as gc
-
+from pyspark.ml.feature import MinMaxScaler
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml import Pipeline
+from pyspark.sql.functions import udf
 
 class Textural:
     def __init__(self, train,spark):
@@ -118,6 +122,30 @@ class Textural:
                                                     "GLCM_Energy(gray_scale) as glcm_energy",
                                                     "GLCM_Corr(gray_scale) as glcm_correlation",
                                                     "GLCM_ASM(gray_scale) as glcm_ASM")
+
+        unlist = udf(lambda x: round(float(list(x)[0]), 3), DoubleType())
+
+        # Iterating over columns to be scaled
+        for i in ["glcm_contrast", "glcm_dissimilarity", "glcm_homogeneity", "glcm_energy", "glcm_correlation",
+                  "glcm_ASM"]:
+            # VectorAssembler Transformation - Converting column to vector type
+            assembler = VectorAssembler(inputCols=[i], outputCol=i + "_Vect")
+
+            # MinMaxScaler Transformation
+            scaler = MinMaxScaler(inputCol=i + "_Vect", outputCol=i + "_Scaled")
+
+            # Pipeline of VectorAssembler and MinMaxScaler
+            pipeline = Pipeline(stages=[assembler, scaler])
+
+            # Fitting pipeline on dataframe
+            glcm_features_df = pipeline.fit(glcm_features_df).transform(glcm_features_df).withColumn(i + "_Scaled",
+                                                                                                     unlist(
+                                                                                                         i + "_Scaled")).drop(
+                i + "_Vect")
+
+        glcm_features_df = glcm_features_df.select("origin", "Geom", "glcm_contrast_Scaled",
+                                                   "glcm_dissimilarity_Scaled", "glcm_homogeneity_Scaled",
+                                                   "glcm_energy_Scaled", "glcm_correlation_Scaled", "glcm_ASM_Scaled")
         return glcm_features_df
 
 
