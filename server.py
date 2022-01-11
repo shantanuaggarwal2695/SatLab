cd882028271e01baf04fe28e1babe96255c6ecdc
shantanuaggarwal <shantanuaggarwal29@gmail.com>
2022-01-11 14:00:17 -0500

    flask server

--- server.py
+++ server.py
@@ -12,14 +12,17 @@ def user():
         data = request.form
         path = data["path"]
         result = run_job(path, spark)
-        response = []
-        record = dict()
-        for row in result:
-            record["id"] = row[0]
-            record["label"] = row[1]
-            response.append(record)
+        print(result)
 
-        return response
+        return {}
+        # response = []
+        # record = dict()
+        # for row in result:
+        #     record["id"] = row[0]
+        #     record["label"] = row[1]
+        #     response.append(record)
+        #
+        # return response
 
 
 if __name__ == '__main__':
